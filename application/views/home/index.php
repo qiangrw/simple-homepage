bb73051369bdcbc9ee739a0028111d4684d04758
Runwei Qiang <qiangrw@gmail.com>
2015-09-23 00:05:57 +0800

    add homepage and publications page

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,7 +1,32 @@
-<div class="container">
-    <div class="starter-template">
-        <h1>Bootstrap starter template</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+<div id="main-container" class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <img src="<?= base_url('images/profile.png') ?>" alt="">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-10">
+            <h1>Runwei Qiang</h1>
+            <p>
+            <a target="_blank" href="http://www.icst.pku.edu.cn/">Institute of Computer Science and Technology</a> of 
+            <a target="_blank" href="http://www.pku.edu.cn/">Peking University</a>
+            </p>
+            <p><strong>Office:</strong> Room 314, 128 Zhongguancun North Street, Haidian, Beijing</p>
+            <p><strong>Email:</strong> qiangrw#pku.edu.cn </p>
+            <p><strong>LinkedIn:</strong> <a target="_blank" href="http://www.linkedin.com/in/qiangrw">http://cn.linkedin.com/in/qiangrw</a> </p>
+            <p><strong>Biography:</strong></p>
+
+
+            <p>
+            Currently I am a third year graduate student of Institute of Computer Science and
+            Technology in Peking University. My research interest is information retrieval.
+
+            <br>
+            I received my B.Sc. degree in Computer Science and Technology in June 2012 from Nanjing
+            University. In September 2012, I was admitted to study for a M.Sc. degree in Peking
+            University without entrance examination.
+            </p>
+        </div>
     </div>
 </div><!-- /.container -->
 
--- application/views/home/publications.php
+++ application/views/home/publications.php
@@ -1,7 +1,64 @@
-<div class="container">
-    <div class="starter-template">
-        <h1>Bootstrap starter template</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+<div id="main-container" class="container">
+	<div class="row">
+    <div class="col-md-12">
+    <p class="lead">Paper</p>
+	<p>
+    <ul>
+        <li><strong>Runwei Qiang</strong>, Feifan Fan, Chao Lv and Jianwu Yang. 
+            Knowledge-based Query Expansion in Real-Time Microblog Search. Arxiv 2015 <a href="http://arxiv.org/pdf/1503.03961v1.pdf">Download</a>
+        </li>
+    
+        <li>Chao Lv, Feifan Fan, <strong>Runwei Qiang</strong>, Yue Fei and Jianwu Yang. 
+            PKUICST at TREC 2014 Microblog Track: <br>
+            Feature Extraction for Effective Microblog Search and
+            Adaptive Clustering Algorithms for TTG. TREC 2014
+        </li>
+        <li><strong>Runwei Qiang</strong>,  Yue Fei, Yihong Hong, and Jianwu Yang. PKUICST at TREC 2013 Microblog Track. TREC 2013.</li>
+        <li><strong>Runwei Qiang</strong>, Feng Liang, Jianwu Yang. Exploiting Ranking Factorization Machines for Microblog Retrieval. CIKM 2013.
+        <a href="../download/paper/cikm2013_qiang_rankingfm.pdf">Download</a></li>
+        <li>Feng Liang, <strong>Runwei Qiang</strong>, Yihong Hong, Yue Fei and Jianwu Yang. PKUICST at TREC 2012 Microblog Track. TREC 2012.</li>
+        <li>Feng Liang, <strong>Runwei Qiang</strong>, and Jianwu Yang. Exploiting Real-Time Information Retrieval in the
+        Microblogosphere. JCDL2012.
+        <a href="../download/paper/jcdl2012_liang_microblog.pdf">Download</a></li>
+        <li>
+        Feng Liang, <strong>Runwei Qiang</strong>, and Jianwu Yang. PKU_ICST at TREC 2011 Microblog Track. TREC 2011.
+        </li>
+    </ul>
+    </p>
+    
+    <br><br>
+    <p class="lead">Patent</p>
+	<table class="table table-bordered table-hover">
+		<thead>
+		<tr>
+			<th>发明名称</th>
+			<th>专利号</th>
+			<th>发明人</th>
+		</tr>
+		</thead>
+        <tbody>
+		
+		<tr>
+            <td>一种基于上下文扩展的微博实时检索方法和装置</td>
+            <td>201410197261.7</td>
+            <td>强闰伟 费跃 杨建武</td>
+        </tr>
+		
+		<tr>
+            <td>一种基于排序分解机模型的微博实时检索方法和装置</td>
+            <td>201410197262.1</td>
+            <td>强闰伟 梁丰 杨建武</td>
+        </tr>
+        
+        <tr>
+			<td>一种微博实时检索的方法及装置</td>
+			<td>201210564284.8</td>
+			<td>梁丰 强闰伟 杨建武</td>
+		</tr>
+		</tbody>
+	</table>
+    
+    </div>
     </div>
 </div><!-- /.container -->
 
