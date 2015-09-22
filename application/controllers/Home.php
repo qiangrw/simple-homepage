e93b41122a08f4c664ea5e4edfd24c010e34fc72
Runwei Qiang <qiangrw@gmail.com>
2015-09-22 23:52:10 +0800

    add basic pages, four nav buttons

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,8 +5,37 @@ class Home extends CI_Controller {
 	public function index()
     {
         $data['title'] = 'Homepage';
+        $data['active'] = array('active', '', '', '');
 		$this->load->view('inc/header', $data);
 		$this->load->view('home/index');
+		$this->load->view('inc/footer');
+    }
+
+    public function publications()
+    {
+        $data['title'] = 'Publications';
+        $data['active'] = array('', 'active', '', '');
+		$this->load->view('inc/header', $data);
+		$this->load->view('home/publications');
+		$this->load->view('inc/footer');
+    }
+
+ 
+    public function experience()
+    {
+        $data['title'] = 'Experience';
+        $data['active'] = array('', '', 'active', '');
+		$this->load->view('inc/header', $data);
+		$this->load->view('home/experience');
+		$this->load->view('inc/footer');
+    }
+
+    public function honors()
+    {
+        $data['title'] = 'Honors';
+        $data['active'] = array('', '', '', 'active');
+		$this->load->view('inc/header', $data);
+		$this->load->view('home/honors');
 		$this->load->view('inc/footer');
 	}
 }
--- application/views/inc/header.php
+++ application/views/inc/header.php
@@ -27,13 +27,14 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Project name</a>
+          <a class="navbar-brand" href="<?= site_url('home') ?>">Runwei</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li class="<?= $active[0] ?>"><a href="<?= site_url('home/index')  ?>">HOME</a></li>
+            <li class="<?= $active[1] ?>"><a href="<?= site_url('home/publications') ?>">PUBLICATIONS</a></li>
+            <li class="<?= $active[2] ?>"><a href="<?= site_url('home/experience') ?>">EXPERIENCE</a></li>
+            <li class="<?= $active[3] ?>"><a href="<?= site_url('home/honors') ?>">HONORS</a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
