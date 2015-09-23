e077eb53d317ebb3dbbff836f38fa99625f8eca8
Runwei Qiang <qiangrw@gmail.com>
2015-09-24 01:41:38 +0800

    add experience description

--- application/views/home/experience.php
+++ application/views/home/experience.php
@@ -32,16 +32,40 @@
                 <li> 
                 <strong>Algorithm Engineer Intern</strong> at <a target="_blank" href="http://yidianzixun.com">Yidian Inc.</a>, 02/2015 - 07/2015 
                 (mentor: <a href="https://sites.google.com/site/jiangbianhome/home" href="_blank">Jiang Bian</a>)
+                <ul>
+                    <li> Improved news similarity calculation by introducing word embedding features.  </li>
+                    <li> Further optimized news similarity service by applying SSE (Streaming SIMD Extensions) and multi-threading techniques.  </li>
+                    <li> Built a feature monitoring site for the head news recommender system.  </li>
+                    <li> Built a system to predict the timeliness of news using linear SVR with various textual features.  </li>
+                </ul>
+
                 </li> 
                 <li> 
                 <strong>Research Intern</strong> at <a target="_blank" href="http://www.msra.cn/zh-cn/default.aspx">Microsoft Research</a>, 02/2014 - 09/2014  
                 (mentor: <a href="http://research.microsoft.com/en-us/people/bingao/" target="_blank">Bin Gao</a>,  
                 <a href="https://sites.google.com/site/jiangbianhome/home" target="_blank">Jiang Bian</a> and 
                 <a href="http://research.microsoft.com/en-us/people/tyliu/" target="_blank">Tie-Yan Liu</a>)
+
+                <ul>
+                    <li>
+                    Built a deep neural network framework to obtain word embedding which provides
+                    important information to benefit relevance ranking task. 
+                    </li>
+                    <li>
+                    Designed and built a knowledge-powered deep neural network framework to leverage text knowledge to address the OOV problem in learning word embedding. 
+                    </li>
+                    <li>
+                    Built a learning to rank framework to combine different word embedding to improve retrieval performance.
+                    </li>
+                </ul>
                 </li> 
                 <li> 
                 <strong>Software Engineer Intern</strong> at <a target="_blank" href="http://hulu.com">Hulu</a>, 07/2012 - 10/2012 
                 (mentor: Chao Li) 
+                <ul>
+                    <li> Work on web audience team. Design site test case for Hulu 2.0 with rspec framework.  </li>
+                    <li>Design and develop RspecDoc with Ruby to help mange test case. Engage in the development of rats, a report system for test cases.  </li>
+                </ul>
                 </li> 
             </ul>
         </div>
