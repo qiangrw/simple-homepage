05643d08d8ba5bc053666e82a1b44f51127aba08
Runwei Qiang <qiangrw@gmail.com>
2015-09-23 10:48:13 +0800

    use asset folder for css and js

--- application/views/inc/footer.php
+++ application/views/inc/footer.php
@@ -1,9 +1,9 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="<?= base_url('js/jquery.min.js') ?>"></script>
-    <script src="<?= base_url('js/bootstrap.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/jquery.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/bootstrap.min.js') ?>"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="<?= base_url('js/ie10-viewport-bug-workaround.js') ?>"></script>
+    <script src="<?= base_url('assets/js/ie10-viewport-bug-workaround.js') ?>"></script>
   </body>
 </html>    
--- application/views/inc/header.php
+++ application/views/inc/header.php
@@ -8,8 +8,8 @@
     <meta name="author" content="Runwei Qiang">
     <!--<link rel="icon" href="../../favicon.ico">-->
     <title><?= $title ?></title>
-    <link href="<?= base_url('css/bootstrap.min.css') ?>" rel="stylesheet">
-    <link href="<?= base_url('css/simple-homepage.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/css/simple-homepage.css') ?>" rel="stylesheet">
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
