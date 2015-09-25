2c4d888e2d773ed1d492c8848c07c92ce321e8f3
Runwei Qiang <qiangrw@gmail.com>
2015-09-25 09:36:54 +0800

    update experience page, add thumbnail for companies and schools

--- application/views/home/experience.php
+++ application/views/home/experience.php
@@ -2,75 +2,133 @@
     <div class="row">
         <div class="col-md-12">
             <h2>Education</h2>
-            <ul>
-                <li>
-                Master Degree in Computer Applied Technology, 2012 - 2015: 
-                <a target="_blank" href="http://www.icst.pku.edu.cn/">Institute of Computer Science and Technology</a>,   
-                <a target="_blank" href="http://www.pku.edu.cn/">Peking University</a>
-                </li>
-
-                <li>
-                Bachelor Degree in Computer Science, 2008 - 2012:  
-                <a target="_blank" href="http://cs.nju.edu.cn/">Department of Computer Science and Technology</a>,
-                <a target="_blank" href="http://www.nju.edu.cn/">Nanjing University</a>
-                </li>
-            </ul>
+            <div class="media">
+                <div class="media-left">
+                    <a href="http://www.pku.edu.cn">
+                        <img class="media-object" src="<?= base_url('images/school/pku.png') ?>" alt="PKU">
+                    </a>
+                </div>
+                <div class="media-body">
+                    <h4 class="media-heading">Peking University</h4>
+                    Master of Science (M.S.),  Computer Applied Technology 
+                    <br>
+                    <span class="text-muted"> 2012 - 2015: </span>
+                    <a target="_blank" href="http://www.icst.pku.edu.cn/">Institute of Computer Science and Technology</a>
+                </div>
+            </div>
+            <div class="media">
+                <div class="media-left">
+                    <a href="http://www.nju.edu.cn">
+                        <img class="media-object" src="<?= base_url('images/school/nju.png') ?>" alt="NJU">
+                    </a>
+                </div>
+                <div class="media-body">
+                    <h4 class="media-heading">Nanjing University</h4>
+                    Bachelor of Science (B.S.),  Computer Science
+                    <br>
+                    <span class="text-muted"> 2008 - 2012: </span>
+                    <a target="_blank" href="http://cs.nju.edu.cn/">Department of Computer Science and Technology</a>
+                </div>
+            </div>
         </div>
     </div>
-
 
     <div class="row">
         <div class="col-md-12">
             <h2>Work Experience</h2>
-            <ul>
-                <li>
-                <strong>Software Engineer</strong> at 
-                <a target="_blank" href="http://google.com">Google Japan</a>,
-                11/2015 - present  
-                (mentor: Tomo Nanno)
-                </li>
-                <li> 
-                <strong>Algorithm Engineer Intern</strong> at <a target="_blank" href="http://yidianzixun.com">Yidian Inc.</a>, 02/2015 - 07/2015 
-                (mentor: <a href="https://sites.google.com/site/jiangbianhome/home" href="_blank">Jiang Bian</a>)
-                <ul>
-                    <li> Improved news similarity calculation by introducing word embedding features.  </li>
-                    <li> Further optimized news similarity service by applying SSE (Streaming SIMD Extensions) and multi-threading techniques.  </li>
-                    <li> Built a feature monitoring site for the head news recommender system.  </li>
-                    <li> Built a system to predict the timeliness of news using linear SVR with various textual features.  </li>
-                </ul>
+            <div class="media">
+                <div class="media-left">
+                    <a href="http://google.com" target="_blank">
+                        <img class="media-object" src="<?= base_url('images/work/google.png') ?>" alt="Google">
+                    </a>
+                </div>
+                <div class="media-body">
+                    <h4 class="media-heading">Software Engineer at Google Japan</h4>
+                    <span class="text-muted">
+                        11/2015 - present 
+                        (mentor: Tomo Nanno)
+                    </span>
+                    <ul>
+                        <li> Google shopping team </li>
+                    </ul>
+                </div>
+            </div> 
 
-                </li> 
-                <li> 
-                <strong>Research Intern</strong> at <a target="_blank" href="http://www.msra.cn/zh-cn/default.aspx">Microsoft Research</a>, 02/2014 - 09/2014  
-                (mentor: <a href="http://research.microsoft.com/en-us/people/bingao/" target="_blank">Bin Gao</a>,  
-                <a href="https://sites.google.com/site/jiangbianhome/home" target="_blank">Jiang Bian</a> and 
-                <a href="http://research.microsoft.com/en-us/people/tyliu/" target="_blank">Tie-Yan Liu</a>)
+            <div class="media">
+                <div class="media-left">
+                    <a href="http://yidianzixun.com" target="_blank">
+                        <img class="media-object" src="<?= base_url('images/work/yidian.png') ?>" alt="Yidian">
+                    </a>
+                </div>
+                <div class="media-body">
+                    <h4 class="media-heading">
+                        Algorithm Engineer Intern at Yidian
+                    </h4>
+                    <span class="text-muted">
+                        02/2015 - 07/2015  
+                        (mentor: <a href="https://sites.google.com/site/jiangbianhome/home" href="_blank">Jiang Bian</a>)
+                    </span>
+                    <ul>
+                        <li> Improved news similarity calculation by introducing word embedding features.  </li>
+                        <li> Further optimized news similarity service by applying SSE (Streaming SIMD Extensions) and multi-threading techniques.  </li>
+                        <li> Built a feature monitoring site for the head news recommender system.  </li>
+                        <li> Built a system to predict the timeliness of news using linear SVR with various textual features.  </li>
+                    </ul>
+                </div>
+            </div> 
 
-                <ul>
-                    <li>
-                    Built a deep neural network framework to obtain word embedding which provides
-                    important information to benefit relevance ranking task. 
-                    </li>
-                    <li>
-                    Designed and built a knowledge-powered deep neural network framework to leverage text knowledge to address the OOV problem in learning word embedding. 
-                    </li>
-                    <li>
-                    Built a learning to rank framework to combine different word embedding to improve retrieval performance.
-                    </li>
-                </ul>
-                </li> 
-                <li> 
-                <strong>Software Engineer Intern</strong> at <a target="_blank" href="http://hulu.com">Hulu</a>, 07/2012 - 10/2012 
-                (mentor: Chao Li) 
-                <ul>
-                    <li> Work on web audience team. Design site test case for Hulu 2.0 with rspec framework.  </li>
-                    <li>Design and develop RspecDoc with Ruby to help mange test case. Engage in the development of rats, a report system for test cases.  </li>
-                </ul>
-                </li> 
-            </ul>
+            <div class="media">
+                <div class="media-left">
+                    <a href="http://www.msra.cn/zh-cn/default.aspx" target="_blank">
+                        <img class="media-object" src="<?= base_url('images/work/ms.png') ?>" alt="Microsfot Research">
+                    </a>
+                </div>
+                <div class="media-body">
+                    <h4 class="media-heading">
+                        Research Intern at Microsoft Research
+                    </h4>
+                    <span class="text-muted">
+                        02/2014 - 09/2014  
+                        (mentor: <a href="http://research.microsoft.com/en-us/people/bingao/" target="_blank">Bin Gao</a>,  
+                        <a href="https://sites.google.com/site/jiangbianhome/home" target="_blank">Jiang Bian</a> and 
+                        <a href="http://research.microsoft.com/en-us/people/tyliu/" target="_blank">Tie-Yan Liu</a>)
+                    </span>
+                    <ul>
+                        <li>
+                        Built a deep neural network framework to obtain word embedding which provides
+                        important information to benefit relevance ranking task. 
+                        </li>
+                        <li>
+                        Designed and built a knowledge-powered deep neural network framework to leverage text knowledge to address the OOV problem in learning word embedding. 
+                        </li>
+                        <li>
+                        Built a learning to rank framework to combine different word embedding to improve retrieval performance.
+                        </li>
+                    </ul>        
+                </div>
+            </div> 
+
+            <div class="media">
+                <div class="media-left">
+                    <a href="http://hulu.com" target="_blank">
+                        <img class="media-object" src="<?= base_url('images/work/hulu.png') ?>" alt="Hulu">
+                    </a>
+                </div>
+                <div class="media-body">
+                    <h4 class="media-heading">
+                        Software Engineer Intern at Hulu
+                    </h4>
+                    <span class="text-muted">
+                        07/2012 - 10/2012 
+                        (mentor: Chao Li) 
+                    </span>
+                    <ul>
+                        <li> Work on web audience team. Design site test case for Hulu 2.0 with rspec framework.  </li>
+                        <li>Design and develop RspecDoc with Ruby to help mange test case. Engage in the development of rats, a report system for test cases.  </li>
+                    </ul> 
+                </div>
+            </div> 
         </div>
     </div>     
-
-
 </div>
 
