bd52a9a802cc350b1931a195a4827f5aa5ef4a2c
Runwei Qiang <qiangrw@gmail.com>
2015-09-28 17:26:51 +0800

    add copyright footer

--- application/views/inc/footer.php
+++ application/views/inc/footer.php
@@ -1,9 +1,16 @@
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="<?= base_url('assets/js/jquery.min.js') ?>"></script>
-    <script src="<?= base_url('assets/js/bootstrap.min.js') ?>"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="<?= base_url('assets/js/ie10-viewport-bug-workaround.js') ?>"></script>
-  </body>
+<div class="container">
+    <footer>
+    <p>&copy; Runwei Qiang 2015</p>
+    </footer>
+</div>
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="<?= base_url('assets/js/jquery.min.js') ?>"></script>
+<script src="<?= base_url('assets/js/bootstrap.min.js') ?>"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="<?= base_url('assets/js/ie10-viewport-bug-workaround.js') ?>"></script>
+</body>
 </html>    
