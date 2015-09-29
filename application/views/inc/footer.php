184ffb8a13405b4c95019d7c2a67238f333f8c5b
Runwei Qiang <qiangrw@gmail.com>
2015-09-29 17:26:30 +0800

    use cdn

--- application/views/inc/footer.php
+++ application/views/inc/footer.php
@@ -9,8 +9,8 @@
 <!-- Bootstrap core JavaScript
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
-<script src="<?= base_url('assets/js/jquery.min.js') ?>"></script>
-<script src="<?= base_url('assets/js/bootstrap.min.js') ?>"></script>
+<script src="http://libs.baidu.com/jquery/1.11.3/jquery.min.js"></script>
+<script src="http://libs.baidu.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
 <script src="<?= base_url('assets/js/stats.js') ?>"></script>
 <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
 <script src="<?= base_url('assets/js/ie10-viewport-bug-workaround.js') ?>"></script>
