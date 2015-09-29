3d496527f1960382ffd72d21ed7f273a678ca4ad
Runwei Qiang <qiangrw@gmail.com>
2015-09-29 09:48:32 +0800

    move baidu tongji to the end of the page

--- application/views/inc/footer.php
+++ application/views/inc/footer.php
@@ -13,5 +13,17 @@
 <script src="<?= base_url('assets/js/bootstrap.min.js') ?>"></script>
 <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
 <script src="<?= base_url('assets/js/ie10-viewport-bug-workaround.js') ?>"></script>
+
+<script>
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "//hm.baidu.com/hm.js?2fcea6afd1315fed5f881e0574095171";
+    var s = document.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(hm, s);
+})();
+</script>
+
+
 </body>
 </html>    
